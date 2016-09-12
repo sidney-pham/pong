ed2737bdb83fe6a6d2b1c99fa5bc3a7db0fb5ce8
sidney-pham <sidneypham1@gmail.com>
2016-09-13 03:03:06 +1000

    There is now a ball.

---
+++ Pong/Forms/Game/Game.vb
@@ -0,0 +1,51 @@
+﻿Partial Public Class Game
+    Public CENTRE_X As Double
+    Public CENTRE_Y As Double
+    Dim PADDLE_SIZE As Integer
+    Dim BALL_SIZE As Integer
+    Dim CIRCLE_RADIUS As Integer
+    Dim BACKGROUND_COLOR As Color
+    Dim speed As Single
+    Dim rndInst As Random
+    Dim xVel As Single
+    Dim yVel As Single
+
+    Dim cursorAngle As Double
+
+    Private Sub preload()
+        ' Basically constants. Can't initialise constants at the very top because form hasn't loaded yet.
+        ' Everything will be a variable, for consistency. (Who needs constants?)
+        CENTRE_X = Me.Width / 2
+        CENTRE_Y = Me.Height / 2
+        PADDLE_SIZE = 40
+        BALL_SIZE = 40
+        CIRCLE_RADIUS = Me.Height / 2 * 0.9
+        BACKGROUND_COLOR = Color.FromArgb(21, 18, 71)
+
+        ' Ball
+        speed = 5 ' Ball Speed
+        rndInst = New Random() ' Random instance
+        xVel = Math.Cos(rndInst.Next(5, 10)) * speed
+        yVel = Math.Sin(rndInst.Next(5, 10)) * speed
+    End Sub
+
+    Private Sub create()
+        ' Set size of ball
+        picBall.Width = BALL_SIZE
+        picBall.Height = BALL_SIZE
+
+        ' Position ball
+        picBall.setX(CENTRE_X)
+        picBall.setY(CENTRE_Y)
+
+        ' Position score
+        lblScore.horizontallyCentre()
+        lblScore.verticallyCentre()
+    End Sub
+
+    Private Sub update()
+        drawPaddle()
+        moveBall()
+        checkCollisions()
+    End Sub
+End Class
---
+++ Pong/Forms/Game/Game_Helpers.vb
@@ -0,0 +1,37 @@
+﻿
+' The really boring stuff.
+Partial Public Class Game
+    Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+        Pong.Menu.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Game_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        preload()
+        create()
+    End Sub
+
+    Private Sub tmrTimer_Tick(sender As Object, e As EventArgs) Handles tmrGame.Tick
+        update()
+    End Sub
+
+    Private Sub drawPaddle()
+        cursorAngle = getMouseAngle(Cursor.Position())
+        Me.Refresh()
+    End Sub
+
+    Private Sub moveBall()
+        picBall.Location = New Point(picBall.Location.X + xVel, picBall.Location.Y + yVel)
+    End Sub
+
+    Private Sub checkCollisions()
+
+    End Sub
+
+    Private Sub Game_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+        ' Actually draw the paddle.
+        Dim rect As New Rectangle(CENTRE_X - CIRCLE_RADIUS, CENTRE_Y - CIRCLE_RADIUS, CIRCLE_RADIUS * 2, CIRCLE_RADIUS * 2)
+        Dim startAngle As Integer = cursorAngle / (2 * Math.PI) * 360 - PADDLE_SIZE / 2
+        e.Graphics.DrawArc(New Pen(Color.Orange, 10), rect, startAngle, PADDLE_SIZE)
+    End Sub
+End Class
